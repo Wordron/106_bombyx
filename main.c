5abb95905b592225a8378b9792548f841c139dee
Wordron <maxime1.leroy@epitech.eu>
2023-02-13 15:55:51 +0100

    error handling

--- main.c
+++ main.c
@@ -31,8 +31,12 @@ int main(int ac, char **av)
         if (my_str_isfloat(av[i]) == 0)
             return (84);
     }
-    if (ac == 3)
+    if (ac == 3) {
         first_option(atof(av[1]), atof(av[2]));
-    if (ac == 4)
+    }
+    if (ac == 4) {
+        if (atoi(av[2]) < 0 | atoi(av[3]) < 0 | atoi(av[2]) <= atoi(av[3]))
+            return (84);
         option_two(atof(av[1]), atoi(av[2]), atoi(av[3]));
+    }
 }
